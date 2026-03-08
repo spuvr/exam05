18c6849f90af4239dec0ac3727d71184137dd69f
Oussama Adouz <adouza.13@gmail.com>
2026-03-08 22:16:10 +0000

    vect2 1

---
+++ taring/lv1/vect2/vect2.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <iostream>
+
+class vect2
+{
+	private:
+		int x, y;
+	public:
+	// constructos (4)
+		vect2() : x(0), y(0) {};
+		vect2(int x, int y) : x(x), y(y) {};
+		vect2(const vect2 &other) : x(other.x), y(other.y) {};
+		vect2 &operator=(const vect2 &other)
+		{
+			if (this != &other)
+			{
+				x = other.x;
+				y = other.y;
+			}
+			return *this;
+		}
+
+	// access (2) [], const []
+		int &operator[](int i)
+		{
+			return (i == 0 ? x : y);
+		}
+		int operator[](int i) const
+		{
+			return (i == 0 ? x : y);
+		}
+	//increment / decrement (4)
+	// ++prefix, --prefix | prefix++, prefix--
+		vect2 &operator++()
+		{
+			x++; y++;
+			return *this;
+		}
+		vect2 operator++(int)
+		{
+			vect2 tmp(*this);
+			x++; y++;
+			return tmp;
+		}
+		vect2 &operator--()
+		{
+			x--; y--;
+			return *this;
+		}
+		vect2 operator--(int)
+		{
+			vect2 tmp(*this);
+			x--; y--;
+			return (tmp);
+		}
+	//compund assignemt (3) | *=. +=. -=
+		vect2 &operator*=(int n)
+		{
+			x *= n;
+			y *= n;
+			return *this;
+		}
+		vect2 &operator +=(const vect2 &other)
+		{
+			x += other.x;
+			y += other.y;
+			return *this;
+		}
+		vect2 &operator-=(const vect2 &other)
+		{
+			x -= other.x;
+			y -= other.y;
+			return *this;
+		}
+	// arithmetic (4) | +, -, unar -, *
+		vect2 operator+(const vect2 &other) const
+		{
+			return vect2(x + other.x, y + other.y);
+		}
+		vect2 operator-(const vect2 &other) const
+		{
+			return vect2(x - other.x, y - other.y);
+		}
+		vect2 operator-() const
+		{
+			return vect2(-x, -y);
+		}
+		vect2 operator*(int n) const
+		{
+			return vect2(x * n , y * n);
+		}
+	//comparison (2), != , ==
+		bool operator==(const vect2 &other) const
+		{
+			return ( x == other.x && y == other.y);
+		}
+		bool operator!=(const vect2 &other) const
+		{
+			return (x != other.x || y != other.y);
+		}
+	// friend (2)
+		friend vect2 operator*(int n, const vect2 &other)
+		{
+			return vect2(other.x * n, other.y * n);
+		}
+		friend std::ostream &operator<<(std::ostream &os, const vect2 &other)
+		{
+			os << "{" << other.x << ", " << other.y << "}";
+			return os;
+		}
+
+};
